e673e00a3e446679fc6652cd9e8bef33ea8dcca2
Mateo Munguia <juanpablomunguiagarcia@MacBook-Pro-de-Mateo-Munguia.local>
2023-02-16 15:11:30 -0400

    initial commit

---
+++ Publisher/Controllers/PublishController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Dapr.Client;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Publisher.Controllers
+{
+   
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PublishController : ControllerBase
+    {
+        private readonly DaprClient _daprClient;
+  
+
+        public PublishController(DaprClient daprClient)
+        {
+            _daprClient = daprClient;
+         
+        }
+
+        [HttpPost("messages")]
+        public async Task<ActionResult> PublishMessageAsync([FromBody]string message)
+        {
+            var pubsubName = "pubsub"; // Reemplaza con el nombre de tu pubsub
+            var topicName = "mytopic"; // Reemplaza con el nombre de tu topic
+
+            await _daprClient.PublishEventAsync(pubsubName,topicName,message);
+           
+          
+            return Ok();
+        }
+    }
+
+}
+
